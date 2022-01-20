96fedd3e5fff8053a757d558a4890cca8854e456
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-20 20:30:01 +0100

    changed the class constructor to argument method

--- prueba.js
+++ prueba.js
@@ -3,6 +3,9 @@ class CoolArray {
     for (let i = 0; i < arguments.length; i++) {
       this[i] = arguments[i];
     }
-    this.length = arguments.length;
   }
 }
+
+const prueba = new CoolArray(1, 3, 4, 5, 5, 6, 7, 8, 23423, 234);
+
+console.log(prueba);
--- push.test.js
+++ push.test.js
@@ -1,13 +1,12 @@
 class CoolArray {
-  index0;
-  index1;
-  index2;
-
-  constructor(value1, value2, value3) {
-    this.index0 = value1;
-    this.index1 = value2;
-    this.index2 = value3;
+  constructor() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[i] = arguments[i];
+    }
+    this.length = arguments.length;
   }
+}
+
 
   push(value) {
     if (this.index0 !== undefined) {
