951b191e15e45bde31b645467f696776736cc0e1
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-20 19:10:19 +0100

    added push method to add 4th number

---
+++ push.test.js
@@ -0,0 +1,15 @@
+class CoolArray {
+  index0;
+  index1;
+  index2;
+
+  constructor(value1, value2, value3) {
+    this.index0 = value1;
+    this.index1 = value2;
+    this.index2 = value3;
+  }
+
+  push(value) {
+    this.index3 = value;
+  }
+}
