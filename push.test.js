069199f1eb6be2582b990b35521f1c9f42f76fad
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-20 20:02:15 +0100

    added push method

--- push.test.js
+++ push.test.js
@@ -24,3 +24,20 @@ class CoolArray {
     }
   }
 }
+
+// describe("Given a push method of CoolArray class", () => {
+//   describe("When it recives the value 3 and it's called from a object with 2 properties", () => {
+//     test("Then it should add a 3rd property with the value 3 called index2", () => {
+//       const objectTest = new CoolArray(1, 2);
+//       const value = 3;
+//       objectTest.index3 = 3;
+
+//       const ReturnedValue = objectTest.push(3);
+
+//       extec
+
+//     })
+
+//   })
+
+// });
