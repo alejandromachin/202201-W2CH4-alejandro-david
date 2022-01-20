b9120e74702107cf42c8135e3b2a59dd814d01e2
David Soto <davidsotosendarrubias@gmail.com>
2022-01-20 20:32:16 +0100

    added lenght codeTest

--- lenght.test.js
+++ lenght.test.js
@@ -1,10 +1,25 @@
-// Function goes here
+function leght(object) {
+  if (this.index0 !== undefined) {
+    if (this.index1 !== undefined) {
+      if (this.index2 !== undefined) {
+        return 3;
+      }
+      return 2;
+    }
+    return 1;
+  }
+  return 0;
+}
 
-describe("Given", () => {
-  describe("When", () => {
-    test("Then", () => {
-      // Arrange
+describe("Given a lengt function", () => {
+  describe("When recived nothing", () => {
+    test("Then return 0", () => {
+      let this.index0;
+      let this.index1;
+      let this.index2;
+       const leghtExpected = 0;
       // Act
+      length()
       // Assert
     });
   });
