7a254490477a23ed5206e9aacf234b0aac5b75b7
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-01-21 08:12:41 +0100

    done tests to push() with 1, 2,3 properties. last one failed

--- push.test.js
+++ push.test.js
@@ -1,7 +1,9 @@
 class CoolArray {
   constructor() {
     for (const i in arguments) {
-      if (Object.hasOwnProperty.call(arguments, i)) {
+      if (arguments[i] === undefined) {
+        this.length = 0;
+      } else {
         this[i] = arguments[i];
         this.length = arguments[i];
       }
@@ -9,25 +11,58 @@ class CoolArray {
   }
 
   push(value) {
-    const index = this.length + 1;
+    const index = this.length;
     this[index] = value;
     this.length = index;
   }
 }
 
-// describe("Given a push method of CoolArray class", () => {
-//   describe("When it recives the value 3 and it's called from a object with 2 properties", () => {
-//     test("Then it should add a 3rd property with the value 3 called index2", () => {
-//       const objectTest = new CoolArray(1, 2);
-//       const value = 3;
-//       objectTest.index3 = 3;
+const prueba1 = new CoolArray();
 
-//       const ReturnedValue = objectTest.push(3);
+prueba1.push("prueba");
+console.log(prueba1);
 
-//       extec
+console.log(prueba1[4]);
 
-//     })
+describe("Given a push method of CoolArray class", () => {
+  describe("When it recives 3 and it's called from a object with 2 properties", () => {
+    test("Then it should add a 3rd property with the value 3", () => {
+      const objectTest = new CoolArray(1, 2);
+      const value = 3;
+      const expectedReturn = 3;
+      objectTest.push(value);
 
-//   })
+      const ReturnedValue = objectTest[2];
 
-// });
+      expect(ReturnedValue).toBe(expectedReturn);
+    });
+  });
+
+  describe("When it recives 'prueba' and it's called from a object with 1 properties", () => {
+    test("Then it should add a 2nd property with the value 'prueba'", () => {
+      const objectTest = new CoolArray(1);
+      const value = "prueba";
+      const expectedReturn = "prueba";
+
+      objectTest.push(value);
+
+      const ReturnedValue = objectTest[1];
+
+      expect(ReturnedValue).toBe(expectedReturn);
+    });
+  });
+
+  describe("When it recives 'prueba' and it's called from a object with 0 properties", () => {
+    test("Then it should add a 1st property with the value 'prueba'", () => {
+      const objectTest = new CoolArray();
+      const value = "prueba";
+      const expectedReturn = "prueba";
+
+      objectTest.push(value);
+
+      const ReturnedValue = objectTest[1];
+
+      expect(ReturnedValue).toBe(expectedReturn);
+    });
+  });
+});
